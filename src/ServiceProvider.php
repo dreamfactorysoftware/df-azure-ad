6e408325d328499ee901e57a309bc3857dab7c88
Lee Hicks <leehicks@dreamfactory.com>
2017-08-10 14:37:23 -0400

    Reworking API doc usage and generation.

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -6,12 +6,9 @@ use DreamFactory\Core\AzureAD\Services\OAuth;
 use DreamFactory\Core\Services\ServiceManager;
 use DreamFactory\Core\Enums\ServiceTypeGroups;
 use DreamFactory\Core\AzureAD\Models\OAuthConfig;
-use DreamFactory\Core\Components\ServiceDocBuilder;
 
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
-    use ServiceDocBuilder;
-
     public function register()
     {
         // Add our service types.
@@ -23,9 +20,6 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
                     'description'     => 'OAuth service for supporting Azure Active Directory authentication and API access.',
                     'group'           => ServiceTypeGroups::OAUTH,
                     'config_handler'  => OAuthConfig::class,
-                    'default_api_doc' => function ($service){
-                        return $this->buildServiceDoc($service->id, OAuth::getApiDocInfo($service));
-                    },
                     'factory'         => function ($config){
                         return new OAuth($config);
                     },
