9fc543eb938f9e6f52778172083e4366cd2b3f96
Lee Hicks <leehicks@dreamfactory.com>
2017-10-26 13:50:38 -0400

    Add subscription requirements to service provider

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -1,26 +1,29 @@
 <?php
+
 namespace DreamFactory\Core\AzureAD;
 
-use DreamFactory\Core\Services\ServiceType;
-use DreamFactory\Core\AzureAD\Services\OAuth;
-use DreamFactory\Core\Services\ServiceManager;
-use DreamFactory\Core\Enums\ServiceTypeGroups;
 use DreamFactory\Core\AzureAD\Models\OAuthConfig;
+use DreamFactory\Core\AzureAD\Services\OAuth;
+use DreamFactory\Core\Enums\LicenseLevel;
+use DreamFactory\Core\Enums\ServiceTypeGroups;
+use DreamFactory\Core\Services\ServiceManager;
+use DreamFactory\Core\Services\ServiceType;
 
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
     public function register()
     {
         // Add our service types.
-        $this->app->resolving('df.service', function (ServiceManager $df){
+        $this->app->resolving('df.service', function (ServiceManager $df) {
             $df->addType(
                 new ServiceType([
-                    'name'            => 'oauth_azure_ad',
-                    'label'           => 'Azure Active Directory OAuth',
-                    'description'     => 'OAuth service for supporting Azure Active Directory authentication and API access.',
-                    'group'           => ServiceTypeGroups::OAUTH,
-                    'config_handler'  => OAuthConfig::class,
-                    'factory'         => function ($config){
+                    'name'                  => 'oauth_azure_ad',
+                    'label'                 => 'Azure Active Directory OAuth',
+                    'description'           => 'OAuth service for supporting Azure Active Directory authentication and API access.',
+                    'group'                 => ServiceTypeGroups::OAUTH,
+                    'subscription_required' => LicenseLevel::SILVER,
+                    'config_handler'        => OAuthConfig::class,
+                    'factory'               => function ($config) {
                         return new OAuth($config);
                     },
                 ])
